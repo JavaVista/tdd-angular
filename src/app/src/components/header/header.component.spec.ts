a67ac6c94f280affbe4c09abf0bd05e6acef847f
Javi <suavejavi@gmail.com>
2020-04-12 15:36:55 -0400

    add test for header and production code to pass

--- src/app/src/components/header/header.component.spec.ts
+++ src/app/src/components/header/header.component.spec.ts
@@ -19,7 +19,15 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show a logo', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="logo"]')).toBeTruthy();
+  });
+
+  it('should show a search field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="search"]')).toBeTruthy();
+  });
+
+  it('should show a menu', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="menu"]')).toBeTruthy();
   });
 });
