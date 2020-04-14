f70c6529a8eb822aed9f6df3f3a660c78f9176e0
Javi <suavejavi@gmail.com>
2020-04-13 23:33:57 -0400

    add test home list in homes page + mock data to pass

--- src/app/src/components/homes/homes.component.ts
+++ src/app/src/components/homes/homes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-homes',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomesComponent implements OnInit {
 
+  homes$;
+
   constructor() { }
 
   ngOnInit() {
+
+    // this.homes$ = this.dataService.getHomes$();
+    // hardcoded service
+    this.homes$ = of([
+      {
+        title: 'Home 1',
+        image: 'assets/listing.jpg',
+        location: 'west palm beach'
+      },
+      {
+        title: 'Home 2',
+        image: 'assets/listing.jpg',
+        location: 'waterbury'
+      },
+      {
+        title: 'Home 3',
+        image: 'assets/listing.jpg',
+        location: 'orlando'
+      }
+    ]);
+
   }
 
 }
